55e9638d095d7c53b5efa3538ace5b5f98d99245
resurrected579 <paulosergio_top.579@gmail.com>
2020-09-14 17:53:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import RNOtpVerify from 'react-native-otp-verify';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Home extends Component {
+  state = {
+    text: ''
+  };
+
+  componentDidMount() {
+    RNOtpVerify.getOtp()
+      .then(p => RNOtpVerify.addListener(this.otpHandler))
+      .catch(p => console.log(p));
+  }
+
+  otpHandler = (message) => {
+    this.onChangeText(message.replace(/\D/g, '').substr(0,4))
+    console.log('SMS :: ',message)
+  }
+
+  onChangeText = (value)=> {
+    this.setState({
+      text: value
+    })
+  }
+
+//   _onPhoneNumberPressed = async () => {
+//     try {
+//       const phoneNumber = await SmsRetriever.requestPhoneNumber();
+//     } catch (error) {
+//       console.log(JSON.stringify(error));
+//     }
+//   };
+//
+// // Get the SMS message (second gif)
+//   _onSmsListenerPressed = async () => {
+//     try {
+//       const registered = await SmsRetriever.startSmsRetriever();
+//       if (registered) {
+//         await SmsRetriever.addSmsListener(event => {
+//           console.log(event.message);
+//           this.onChangeText(event.message.replace(/\D/g, ''))
+//
+//           SmsRetriever.removeSmsListener();
+//         });
+//       }
+//     } catch (error) {
+//       console.log(JSON.stringify(error));
+//     }
+//   };
+
+  render() {
+    return (
+      <ScrollView contentInsetAdjustmentBehavior="automatic"
+                  contentContainerStyle= {styles.scrollView}>
+
+        <Text>Enter One Time Password</Text>
+        <TextInput style= {styles.textInput}
+                   placeholder = "OTP"
+                   autoFocs
+                   onChangeText= {this.onChangeText}
+                   value= {this.state.text} />
+        <TouchableOpacity onPress={() => {}}>
+          <Text>Enter</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scrollView: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  textInput: {
+    marginTop: 20,
+    width:  200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
--- android/app/src/main/java/com/smsautomatic/MainApplication.java
+++ android/app/src/main/java/com/smsautomatic/MainApplication.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import me.furtado.smsretriever.RNSmsRetrieverPackage;
+import com.faizal.OtpVerify.RNOtpVerifyPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
